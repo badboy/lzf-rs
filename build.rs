7c2d9fc48f7cf7fc000e0875438348e4ad7cb4e4
Jan-Erik Rediger <badboy@archlinux.us>
2014-11-22 14:27:20 +0100

    Use gcc crate to build the library

--- build.rs
+++ build.rs
@@ -1,12 +1,8 @@
-use std::io::Command;
-use std::os;
+extern crate gcc;
+use std::default::Default;
 
 fn main() {
-    let out_dir = os::getenv("OUT_DIR").unwrap();
-
-    // note that there are a number of downsides to this approach, the comments
-    // below detail how to improve the portability of these commands.
-    Command::new("make").args(&["-C", "lzf"]).status().unwrap();
-
-    println!("cargo:rustc-flags=-L {} -l lzf:static", out_dir);
+    gcc::compile_library("liblzf.a",
+                         &Default::default(),
+                         &["lzf/lzf_c.c", "lzf/lzf_d.c"]);
 }
