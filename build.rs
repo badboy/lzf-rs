d15b06b1e4e840eb3d9d6d8f058eea304e2b39a4
Jan-Erik Rediger <janerik@fnordig.de>
2015-02-14 11:56:59 +0100

    Use latest gcc crate

--- build.rs
+++ build.rs
@@ -1,8 +1,5 @@
 extern crate gcc;
-use std::default::Default;
 
 fn main() {
-    gcc::compile_library("liblzf.a",
-                         &Default::default(),
-                         &["lzf/lzf_c.c", "lzf/lzf_d.c"]);
+    gcc::compile_library("liblzf.a", &["lzf/lzf_c.c", "lzf/lzf_d.c"]);
 }
